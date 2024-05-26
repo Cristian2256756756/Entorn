45248180750b2e806375ce80651ccd5bd1ebc885
Cristian Valentin Anghel <C.Anghel.sallegi@gmail.com>
2024-05-26 08:54:33 -0400

    Quitado archivos inecesarios que solapan todo, intento de favicon y mas

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import './App.scss';
-import Menu from '../navegacio';
 import ReactDOM from 'react-dom/client';
-import Main from '../parallaxmenu';
 import Navbar from '../components/Navbar/Navbar';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <Navbar /> {/* Correctly add the Navbar component */}
-      <Menu />
-      <Main />
+      <Navbar /> 
+  
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './Navbar.css'
+import {assets} from '../../assets/assets'
 
 const Navbar = () => {
     return(
-        <div className='app'> 
-          
+        <div className='navbar'> 
+          <img src={assets.logo} alt="" className="logo" />
+          <ul className="navbar-menu">
+            <li>Home</li>
+            <li>Menu</li>
+            <li>Mobile-app</li>
+            <li><a href="/About">About</a></li>
+          </ul>
+          <div className="navbar-right">
+            <img src={assets.search_icon} alt=""/>
+            <div className="navbar-search-icon">
+                <img src={assets.basket_icon} alt=""/>
+                <div className="dot"></div>
+            </div>
+            <button>Sign in</button>
+          </div>
         </div>
     )
 }
