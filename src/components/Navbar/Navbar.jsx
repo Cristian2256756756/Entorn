c4914b657bdd655f1de046d427812502134d97e4
Cristian Valentin Anghel <C.Anghel.sallegi@gmail.com>
2024-05-26 10:05:58 -0400

    Logo cambiado, menu navbar cambiado, creado statement home, tocado mas css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState } from 'react'
+import './Navbar.scss'
 import {assets} from '../../assets/assets'
 
 const Navbar = () => {
+
+  const[menu, setMenu] = useState("Home");                       /*Para poder poner una linea debajo de la opcion que estemos del menu*/
+
     return(
         <div className='navbar'> 
           <img src={assets.logo} alt="" className="logo" />
           <ul className="navbar-menu">
-            <li>Home</li>
-            <li>Menu</li>
-            <li>Mobile-app</li>
-            <li><a href="/About">About</a></li>
+            <li on onClick ={()=>setMenu("Home")} className={menu==="Home"?"active":""}>Home</li>
+            <li on onClick ={()=>setMenu("Menu")} className={menu==="Menu"?"active":""}>Menu</li>
+            <li on onClick ={()=>setMenu("Mobile-app")} className={menu==="Mobile-app"?"active":""}>Mobile-app</li>
+            <li on onClick ={()=>setMenu("Contact-us")} className={menu==="Contact-us"?"active":""}>Contact-us</li>
+            <li on onClick ={()=>setMenu("About")} className={menu==="About"?"active":""}><a href="/About">About</a></li>
           </ul>
+
           <div className="navbar-right">
             <img src={assets.search_icon} alt=""/>
             <div className="navbar-search-icon">
