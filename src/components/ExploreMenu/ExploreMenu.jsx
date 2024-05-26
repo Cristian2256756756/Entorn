0942fa5e07a3566f426e1c338a6cd08e2f3bd7a8
Cristian Valentin Anghel <C.Anghel.sallegi@gmail.com>
2024-05-26 14:04:53 -0400

    Menu creado con items, los coje del script de assets, .active creado para cuando escojas un tipo de producto

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './ExploreMenu.scss';
 import { menu_list } from '../../assets/assets';
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category,setCategory}) => {
   return (
     <div className='Explore-Menu' id='Explore-menu'>
       <h1>Explore our menu</h1>
@@ -12,13 +12,14 @@ const ExploreMenu = () => {
       <div className="Explore-Menu-List">
         {menu_list.map((item, index) => {
           return(
-              <div key={index} className='Explore-Menu-Item'>
-                  <img src={item.menu_image} alt=""/>
+              <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='Explore-Menu-List-Item'>
+                  <img className={category===item.menu_name?"active":""} src={item.menu_image} alt=""/>
                   <p>{item.menu_name}</p>
               </div>
           )
         })}
       </div>
+      <hr />
     </div>
   );
 }
