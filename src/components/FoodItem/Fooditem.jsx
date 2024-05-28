682de6868a1ae1332654a3e6fad00864dbc6d684
Cristian Valentin Anghel <C.Anghel.sallegi@gmail.com>
2024-05-28 16:00:08 -0400

    onClick para añadir productos a nuestro basquet, con preve y su propia clase, estira foto de assets

--- src/components/FoodItem/Fooditem.jsx
+++ src/components/FoodItem/Fooditem.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Fooditem.scss'
 import { assets } from '../../assets/assets'
 
 const Fooditem = ({id,name,price,description,image}) => {
 
+  const[itemCount,setItemCount] = useState(0)                                                                     /*Iniciar el carrito de compras con 0 items*/ 
+
   return (
     <div className='food-item'>
       <div className="food-item-img-container">
          <img className='food-item-image' src={image} alt="" />
+         {!itemCount                                                                                               /*El ! es para que el itemCount no sea 0*/ 
+            ?<img className='add' onClick={()=> setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt="" />  /*Le añadimos un signo de + debajo de cada item par que lo podamos meter en el carrito*/
+            :<div className='food-item-counter'>
+
+            </div>
+         }
       </div>
       <div className="food-item-info">
           <div className="food-item-name-rating">
