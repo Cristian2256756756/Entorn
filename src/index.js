5c21ca6e9d235104c607b5244e355b18973c35c0
Cristian Valentin Anghel <C.Anghel.sallegi@gmail.com>
2024-05-28 12:52:25 -0400

    API de los items de la comdia creada, StoreContextProvider metida en index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.scss';
 import App from './App';
 import AboutmeCristian from './aboutme';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import StoreContextProvider from './context/StoreContext';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <StoreContextProvider>
+     <App/>
+    </StoreContextProvider>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="about" element={<AboutmeCristian />} />
+         <Route path="about" element={<AboutmeCristian />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
