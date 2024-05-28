c812ad9f1b18fef8aa345dc4aa2aa652d15c3bec
Cristian Valentin Anghel <C.Anghel.sallegi@gmail.com>
2024-05-28 15:39:39 -0400

    Arreglado errores de index en display. Css de los items tocados, para que se vea mas bonitos,

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -4,15 +4,24 @@ import { StoreContext } from '../../context/StoreContext';
 import Fooditem from '../FoodItem/Fooditem';
 
 const FoodDisplay = ({ category }) => {
-    const { food_list } = useContext(StoreContext);
+    const { food_list } = useContext(StoreContext);                                                       /*Coje todos los datos de food_list que mapeamos mas abajo*/
+
+    const filteredFoodList = category ? food_list.filter(food => food.category === category) : food_list;
 
     return (
         <div className='food-display' id='food-display'>
             <h2>Top dishes near you</h2>
             <div className="food-display-list">
-                {food_list.map((item) => {
-                    return <Fooditem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image}/>
-                })}
+                {filteredFoodList.map((item, index) => (
+                    <Fooditem 
+                        key={item._id} 
+                        id={item._id} 
+                        name={item.name} 
+                        description={item.description} 
+                        price={item.price} 
+                        image={item.image} 
+                    />
+                ))}
             </div>
         </div>
     );
--- src/components/FoodItem/Fooditem.jsx
+++ src/components/FoodItem/Fooditem.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import './Fooditem.scss'
+import { assets } from '../../assets/assets'
 
 const Fooditem = ({id,name,price,description,image}) => {
-  return (
-    <div>
 
+  return (
+    <div className='food-item'>
+      <div className="food-item-img-container">
+         <img className='food-item-image' src={image} alt="" />
+      </div>
+      <div className="food-item-info">
+          <div className="food-item-name-rating">
+            <p>{name}</p>
+            <img src={assets.rating_starts} alt="" />
+          </div>
+          <p className="food-item-descr">{description}</p>
+          <p className="food-item-price">${price}</p>
+      </div>
     </div>
   )
 }
 
 export default Fooditem
+
